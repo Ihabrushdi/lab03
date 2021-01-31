7245d43703022ac5fb5cdedffb62eee9b90cdb52
ihabrushdi <alterawiehab@gmail.com>
2021-01-31 14:33:00 +0200

    adding js

---
+++ app.js
@@ -0,0 +1,10 @@
+var userName = prompt('what is your name');
+// console.log(userName);
+document.write(userName)
+var age = prompt('how old are you ?')
+
+if(age >= 20){
+    alert("welcome to my site")
+}else if(age <20){
+    alert(' sorry this site will not be for you')
+}
